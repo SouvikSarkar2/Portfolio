ddd89850b0760fb93f2880a535ffeb251dbcac6c
Souvik Sarkar <12345souvik12345@gmail.com>
2024-01-06 15:38:00 +0530

    Loader added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "./app.scss";
 import Contact from "./components/contact/Contact";
 import Cursor from "./components/cursor/Cursor";
@@ -6,26 +7,42 @@ import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
 import Projects from "./components/projects/Projects";
 import TechStack from "./components/services/Services";
+import Loader from "./components/Loader/Loader";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const customLoaderTimeout = setTimeout(() => {
+      setLoading(false);
+    }, 6000);
+    return () => clearTimeout(customLoaderTimeout);
+  }, []);
+
   return (
     <div>
-      <Cursor />
-      <section id="Homepage">
-        <Navbar />
-        <Hero />
-      </section>
-      <section id="Services">
-        <Parallax type="services" />
-      </section>
-      <section>
-        <TechStack />
-      </section>
-      <span id="Projects" />
-      <Projects />
-      <section id="Contact">
-        <Contact />
-      </section>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Cursor />
+          <section id="Homepage">
+            <Navbar />
+            <Hero />
+          </section>
+          <section id="Services">
+            <Parallax type="services" />
+          </section>
+          <section>
+            <TechStack />
+          </section>
+          <span id="Projects" />
+          <Projects />
+          <section id="Contact">
+            <Contact />
+          </section>
+        </>
+      )}
     </div>
   );
 };
